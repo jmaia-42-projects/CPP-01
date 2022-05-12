65e99e5c41575f79689f9573a8e352128cb9fc34
Jeremy Maia <jmaia@student.42.fr>
2022-05-12 12:59:57 +0200

    ex02

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 12:54:18 by jmaia             #+#    #+#             */
+/*   Updated: 2022/05/12 12:58:56 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "Memory address of str: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value pointed by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value held by stringREF: " << stringREF << std::endl;
+}
